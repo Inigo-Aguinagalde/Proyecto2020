6507a9b8582241500eb128b1fdf8b3af7698974f
2dam3 <ikbem@plaiaundi.net>
2021-01-29 12:34:01 +0100

    cambios

--- ACDD/Proyecto2020/src/main/java/Proyecto2020/Controller/LocalizacionesController.java
+++ ACDD/Proyecto2020/src/main/java/Proyecto2020/Controller/LocalizacionesController.java
@@ -1,5 +1,6 @@
 package Proyecto2020.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- ACDD/Proyecto2020/src/main/java/Proyecto2020/Controller/RutasController.java
+++ ACDD/Proyecto2020/src/main/java/Proyecto2020/Controller/RutasController.java
@@ -37,19 +37,14 @@ public class RutasController {
 		return RR.save(r);
 	}
 
-<<<<<<< HEAD
 	@GetMapping("/findById")
 	public Rutas findById(@RequestParam String id){
 		Rutas r = RR.findById(id).orElse(null);
 		return r;
 	}
 
-	/*@RequestMapping("/updateCiudad")
-	public @ResponseBody Rutas updateRuta(@RequestParam String id, String nombre, String duracion, String id_localizaciones,int km,String ciudad) {
-=======
 	@RequestMapping("/updateCiudad")
 	public @ResponseBody Rutas updateRuta(@RequestParam String id, String nombre, String duracion, String id_localizaciones,int km,String ciudad,ArrayList lista_puntos) {
->>>>>>> f25f1836c69b5d0a7bc47ad75940b738eb4e5187
 
 		Rutas r = RR.findById(id).orElse(null);
 
@@ -66,7 +61,7 @@ public class RutasController {
 		}
 
 		return r;
-	}*/
+	}
 
 	@GetMapping("/byId")
 	public Rutas getRuta(@RequestParam String id){
